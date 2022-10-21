b6c231e4d7585ba00bb3534b42637f14376e0b0d
Tăng Hiến Phấn <51800593@student.tdtu.edu.vn>
2022-10-21 17:57:41 +0700

    Add resources, collections

---
+++ app/Http/Resources/BookDetailsResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookDetailsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'category' => new CategoryResource($this->category),
+            'author' => new AuthorResource($this->author),
+            'book_title' => $this->book_title,
+            'book_summary' => $this->book_summary,
+            'book_price' => $this->book_price,
+            'book_cover_photo' => $this->book_cover_photo,
+            'discount_price' => $this->discount_price,
+            'discount_start_date' => $this->discount_start_date,
+            'discount_end_date' => $this->discount_end_date,
+            'discount_amount' => $this->getDiscountAmount(),
+            'final_price' => $this->getFinalPrice(),
+        ];
+    }
+}
