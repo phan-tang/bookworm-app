c5578d85323e54a65959656531e09e34250fe8bb
Tăng Hiến Phấn <51800593@student.tdtu.edu.vn>
2022-10-24 15:38:05 +0700

    Add scopes and comments

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,8 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Carbon;
-use Illuminate\Http\Request;
 
 class Book extends Model
 {
@@ -36,45 +34,116 @@ class Book extends Model
     }
 
     /**
-     * Scope a query to show all books with detail.
+     * Scope a query to show all books.
      *
      * @param  \Illuminate\Database\Eloquent\Builder  $query
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeAllBookDetails($query)
+    public function scopeGetAllBookDetails($query)
     {
         return $query
             ->leftJoin('review', 'book.id', '=', 'review.book_id')
             ->leftJoin('discount', 'book.id', '=', 'discount.book_id')
-            ->select('book.*', 'discount.discount_price', 'discount.discount_start_date', 'discount.discount_end_date')
+            ->select(
+                'book.id',
+                'book.category_id',
+                'book.author_id',
+                'book.book_title',
+                'book.book_summary',
+                'book.book_price',
+                'book.book_cover_photo',
+                'discount.discount_price',
+                'discount.discount_start_date',
+                'discount.discount_end_date'
+            )
             ->groupBy('book.id', 'discount.discount_price', 'discount.discount_start_date', 'discount.discount_end_date');
     }
 
-    public function getFinalPrice()
+    /**
+     * Scope a query to select discount amount of a book.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeSelectDiscountAmount($query)
     {
-        $currentDate = Carbon::now()->format('Y-m-d');
-
-        $this->final_price = $this->book_price;
-        if ($this->discount_price != null and $this->discount_start_date <= $currentDate) {
-            if ($this->discount_end_date == null or $currentDate <= $this->discount_end_date) {
-                $this->final_price = $this->discount_price;
-            }
-        }
-
-        return $this->final_price;
+        return $query
+            ->selectRaw("(CASE 
+                        WHEN discount.discount_price = NULL THEN 0
+                        ELSE CASE
+                            WHEN discount.discount_start_date > CURRENT_DATE THEN 0
+                            ELSE CASE
+                                WHEN discount.discount_end_date = NULL THEN book.book_price - discount.discount_price
+                                ELSE CASE
+                                    WHEN discount.discount_end_date >= CURRENT_DATE THEN book.book_price - discount.discount_price
+                                    ELSE 0
+                                    END
+                                END
+                            END
+                        END) AS discount_amount");
     }
 
-    public function getDiscountAmount()
+    /**
+     * Scope a query to select final price of a book.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeSelectFinalPrice($query)
     {
-        $currentDate = Carbon::now()->format('Y-m-d');
+        return $query
+            ->selectRaw("(CASE 
+                        WHEN discount.discount_price = NULL THEN book.book_price
+                        ELSE CASE
+                            WHEN discount.discount_start_date > CURRENT_DATE THEN book.book_price
+                            ELSE CASE
+                                WHEN discount.discount_end_date = NULL THEN discount.discount_price
+                                ELSE CASE
+                                    WHEN discount.discount_end_date >= CURRENT_DATE THEN discount.discount_price
+                                    ELSE book.book_price
+                                    END
+                                END
+                            END
+                        END) AS final_price");
+    }
 
-        $this->discount_amount = 0;
-        if ($this->discount_price != null and $this->discount_start_date <= $currentDate) {
-            if ($this->discount_end_date == null or $currentDate <= $this->discount_end_date) {
-                $this->discount_amount = $this->book_price - $this->discount_price;
-            }
-        }
+    /**
+     * Scope a query to select average rating star of a book.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeSelectAverageRatingStar($query)
+    {
+        return $query
+            ->selectRaw("AVG(COALESCE(review.rating_start, 0)) AS average_rating_star");
+    }
 
-        return $this->discount_amount;
+    /**
+     * Scope a query to select number of reviews of a book.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeSelectNumberOfReviews($query)
+    {
+        return $query
+            ->selectRaw("COUNT(review.rating_start) AS number_of_reviews");
+    }
+
+    /**
+     * Scope a query to display list of books.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeDisplayBooks($query, $limit, $perPage)
+    {
+        return $query->when($limit != null, function ($query) use ($limit) {
+            return $query->limit($limit)->get();
+        })
+            ->when($limit == null, function ($query) use ($perPage) {
+                return $query->paginate($perPage);
+            });
     }
 }
