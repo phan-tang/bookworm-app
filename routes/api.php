40ed3bdce289965bf2064a7a3c8d78ddf3a23e55
Tăng Hiến Phấn <51800593@student.tdtu.edu.vn>
2022-10-26 01:47:28 +0700

    Modify some APIs

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BookAPIController;
+use App\Http\Controllers\ReviewAPIController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('book', BookAPIController::class)->only([
+    'show'
+]);
+
+Route::get('/books', [BookAPIController::class, 'showBooksApplySortFilter']);
+
+Route::get('/reviews', [ReviewAPIController::class, 'showReviews']);
+Route::get('/reviews/sortBy/date', [ReviewAPIController::class, 'showBookReviewsSortByDate']);
