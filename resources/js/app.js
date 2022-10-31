f5c74b35554f52f939a984fa8f60045eb77ee13d
Tăng Hiến Phấn <51800593@student.tdtu.edu.vn>
2022-11-01 00:32:45 +0700

    Install ReactJS in project, import libraies

---
+++ resources/js/app.js
@@ -0,0 +1,55 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes React and other helpers. It's a great starting point while
+ * building robust, powerful web applications using React + Laravel.
+ */
+
+import './bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Outlet
+} from "react-router-dom";
+import '../css/app.scss';
+/**
+ * Next, we will create a fresh React component instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+import Home from './components/Page/Home/Home';
+import Shop from './components/Page/Shop/Shop';
+import About from './components/Page/About/About';
+import Cart from './components/Page/Cart/Cart';
+import Header from './components/Page/Header/Header';
+import Footer from './components/Page/Footer/Footer';
+import 'font-awesome/css/font-awesome.min.css';
+
+function App() {
+    return (
+        <Router>
+            <Header></Header>
+            <div className='container-fluid p-0'>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/shop" element={<Shop />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/cart" element={<Cart />} />
+                </Routes>
+            </div>
+            <Footer></Footer>
+        </Router>
+    );
+}
+
+export default App;
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
