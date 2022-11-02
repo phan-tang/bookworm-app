3aa784bc86a5e6a2f694d6d7dbb19bc622c28c1d
Tăng Hiến Phấn <51800593@student.tdtu.edu.vn>
2022-11-02 15:46:16 +0700

    Add dropdown component

--- resources/js/components/element/dropdown/dropdown.js
+++ resources/js/components/element/dropdown/dropdown.js
@@ -1,15 +1,22 @@
-import { React } from "react";
-
+import { React, useState } from "react";
 function Dropdown(props) {
+
+    const [show, setShow] = useState(props.fields.show);
+
+    //Function to handle click button apply sort or show for list of books
+    const handleClickButtonItem = (field) => {
+        setShow(field.display);
+        props.handleParams(field.params);
+    }
 
     return (
         <span className="dropdown">
             <button className="dropdown-button button" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                {props.fields.show} <i className="fa fa-caret-down"></i>
+                {show} <i className="fa fa-caret-down"></i>
             </button>
             <ul className="dropdown-menu">
                 {props.fields.values.map((field) => {
-                    return (<li key={field.display.toLowerCase().replace(" ", "_")}><button className="button-in-list">{field.display}</button></li>);
+                    return (<li key={field.display.toLowerCase().replace(" ", "_")}><button className="button-in-list" onClick={() => handleClickButtonItem(field)}>{field.display}</button></li>);
                 })}
             </ul>
         </span>
